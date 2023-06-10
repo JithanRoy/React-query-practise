586988ec5d9efe224a56249c12a39daaaec8f39f
Jithan Roy <jithanroyjony@gmail.com>
2023-06-10 09:37:49 +0100

    last update

--- src/components/superHeroes.page.js
+++ src/components/superHeroes.page.js
@@ -1,6 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 export const SuperHeroesPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superHeroes').then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    })
+  }, [])
+
+  if(isLoading) {
+    return <h2>Loading...</h2>
+  }
+
     return (
-      <div>RQSuperHeroes.page</div>
+      <>
+        <h2>Super Heros Page</h2>
+        {
+          data.map((hero, index) => {
+            return <div key={index}>{hero.name}</div>
+          })
+        }
+      </>
     )
   }
   
